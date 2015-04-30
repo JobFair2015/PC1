6e59a53789625897142db6ecc7ac5cbc9095e7a7
JobFair2015 <dejan_bt91@hotmail.com>
2015-04-30 11:41:37 +0200

    initial commit

---
+++ src/main/java/com/jobfair2015/app/App.java
@@ -0,0 +1,13 @@
+package com.jobfair2015.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Real World!" );
+    }
+}
