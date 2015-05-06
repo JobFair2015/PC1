2b4fef4fb8372666bc63e12906c07bef0fe77eb9
JobFair2015 <dejan_bt91@hotmail.com>
2015-05-06 14:33:18 +0200

    project updated

---
+++ src/main/java/com/jobfair2015/app/Greeting.java
@@ -0,0 +1,20 @@
+package com.jobfair2015.app;
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Greeting {
+	
+	
+	//Sample comment
+	public static String greetWorld(){
+		//TO DO
+		return null;
+	}
+	
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/java/com/jobfair2015/app/GreetingTest.java
@@ -0,0 +1,15 @@
+package com.jobfair2015.app;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Test for simple GreetingTest.
+ */
+public class GreetingTest{
+		
+	@Test
+	public void sample(){
+		assertNull(Greeting.greetWorld());
+	}	
+}
